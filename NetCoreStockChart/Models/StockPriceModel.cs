8e1b0f10dcb140ac20662700432f9679840a65bd
lfarul <LFarulewski@yahoo.com>
2020-06-17 17:55:51 +0200

    Add project files.

---
+++ NetCoreStockChart/Models/StockPriceModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoreStockChart.Models
+{
+    public class StockPriceModel
+    {
+        public string Ticker { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Start Date")]
+        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}")]
+        public DateTime StartDate { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "End Date")]
+        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}")]
+        public DateTime EndDate { get; set; }
+
+        [DataType(DataType.Date)]
+        public string Date { get; set; }
+        public decimal Open { get; set; }
+        public decimal High { get; set; }
+        public decimal Low { get; set; }
+        public decimal Close { get; set; }
+        public decimal AdjustedClose { get; set; }
+        public decimal Volume { get; set; }
+    }
+}
